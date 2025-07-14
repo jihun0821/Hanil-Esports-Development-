104e66b63fe53e50701d69531a433cccbfd1e0d4
jihun0821 <afgetre0213@gmail.com>
2025-07-14 23:02:42 +0900

    Delete score.js

--- score.js
+++
@@ -1,5 +0,0 @@
-setInterval(() => {
-  // 예시: 점수를 랜덤으로 바꾸기 (실제론 API 연결 가능)
-  document.getElementById('blue-score').textContent = Math.floor(Math.random() * 10);
-  document.getElementById('red-score').textContent = Math.floor(Math.random() * 10);
-}, 2000);
